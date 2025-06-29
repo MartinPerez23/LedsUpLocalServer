2349843b15abc73b35c8f029c6d376c8b771b8e6
MartinPerez23 <martin.perez@davinci.edu.ar>
2025-06-29 00:05:12 -0300

    Update Executable

--- principal.py
+++ principal.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
+
 import globales
 import vistas.vista_login as login
 import vistas.vista_principal as app_view
 
-
 if __name__ == "__main__":
+    load_dotenv(dotenv_path=globales.resource_path('.env'))
+
     app = login.Login()
     app.mainloop()
 
