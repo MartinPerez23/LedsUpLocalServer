4fde0e29cf33d4be98e3c1c1bb0e666c5ed7aa31
Ivan Parisi <ivanparisi@outlook.es>
2025-07-06 17:13:50 -0300

    fix EXE not show Autenticado correctamente.

--- auth_token/oauth_callback_server.py
+++ auth_token/oauth_callback_server.py
@@ -4,7 +4,6 @@ from urllib.parse import urlparse, parse_qs
 
 PORT = 34123
 
-
 class OAuthCallbackHandler(http.server.BaseHTTPRequestHandler):
     code = None
 
@@ -16,13 +15,16 @@ class OAuthCallbackHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(302)
             self.send_header('Location', 'https://ledsupwebserver.onrender.com/ledsup/autenticado/')
             self.end_headers()
+        elif self.path == "/favicon.ico":
+            self.send_response(204)  # No Content
+            self.end_headers()
         else:
             self.send_response(404)
             self.end_headers()
 
-
 def run_server():
     with socketserver.TCPServer(("127.0.0.1", PORT), OAuthCallbackHandler) as httpd:
-        print(f"Escuchando en https://localhost:{PORT}/callback ...")
-        httpd.handle_request()
+        print(f"Escuchando en http://localhost:{PORT}/callback ...")
+        while OAuthCallbackHandler.code is None:
+            httpd.handle_request()
         return OAuthCallbackHandler.code
