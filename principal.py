e065b30b8048c71672f3a81d69dc8ee13005f37e
MartinPerez23 <martin.perez@davinci.edu.ar>
2025-07-10 10:59:58 -0300

    funcionaaaa

--- principal.py
+++ principal.py
@@ -1,5 +1,3 @@
-import sys
-
 from dotenv import load_dotenv
 
 import globales
@@ -7,15 +5,6 @@ import vistas.vista_login as login
 import vistas.vista_principal as app_view
 
 if __name__ == "__main__":
-    print("nuitka_onefile_compression?", getattr(sys, "nuitka_onefile_compression", False))
-    print("nuitka_compiled?", getattr(sys, "nuitka_compiled", False))
-    print("executable:", sys.executable)
-    print("__file__:", __file__)
-
-    for attr in dir(sys):
-        if "nuitka" in attr.lower():
-            print(f"{attr} = {getattr(sys, attr)}")
-
     load_dotenv(dotenv_path=globales.resource_path('.env'))
 
     app = login.Login()
